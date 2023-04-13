39e016b747118eab5adfc8234daec62c4f734a65
kyobrien <kyobrien@microsoft.com>
2023-04-13 19:50:03 +0000

    Found models which perform okay

--- v2_edit_experiment.py
+++ v2_edit_experiment.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from sklearn.metrics import classification_report
+from tqdm import tqdm
 import pandas as pd
 import torch
 import os
@@ -11,12 +12,14 @@ def get_formatted_dataset(set_name, sample_size=None):
     hf_paths = {
         "toxigen": "skg/toxigen-data",
         "disaster_tweets": "venetis/disaster_tweets",
-        "amazon_polarity": "amazon_polarity"
+        "amazon_polarity": "amazon_polarity",
+        "imdb": "imdb"
     }
     hf_sets_columns_mappings = {
         "toxigen": ("prompt", "prompt_label"),
         "disaster_tweets": ("text", "target"),
         "amazon_polarity": ("content", "label"),
+        "imdb": ("text", "label")
     }
     hf_subset_name_mappings = {
         "toxigen": "train",
@@ -25,7 +28,7 @@ def get_formatted_dataset(set_name, sample_size=None):
 
     hf_dataset = None
     if set_name in hf_subset_name_mappings:
-        hf_dataset = load_dataset(hf_paths[set_name], hf_subset_name_mappings[set_name])["train"].to_pandas()
+        hf_dataset = load_dataset(hf_paths[set_name], hf_subset_name_mappings[set_name], use_auth_token=True)["train"].to_pandas()
     else:
         hf_dataset = load_dataset(hf_paths[set_name])["train"].to_pandas()
 
@@ -54,33 +57,57 @@ def get_complete_prompt(input_sequence, dataset_name, prompt_strategy, num_shots
     def populate_examples(example_sequence, example_label):
         return [f"\nSequence: {example_sequence}\nLabel:{example_label}" for _ in range(num_shots)]
 
-    example_prompt = None
-    example_label = None
     if prompt_strategy == "default":
         default_examples = {
-            "toxigen": {
-                "prompt": "I hate it that muslims are now taking over the US",
-                "label": 1
+            "positive": {
+                "toxigen": {
+                    "prompt": "I hate it that muslims are now taking over the US",
+                    "label": 1
+                },
+                "disaster_tweets": {
+                    "prompt": "There was a suicide bombing in the city last night",
+                    "label": 1
+                },
+                "amazon_polarity": {
+                    "prompt": "I love this product experience",
+                    "label": 1
+                },
+                "imdb": {
+                    "prompt": """ddd"""
+                    ,
+                    "label": 1
+                }
             },
-            "disaster_tweets": {
-                "prompt": "The sky was ablaze last night in beutiful colors",
-                "label": 0
-            },
-            "amazon_polarity": {
-                "prompt": "I love this product experience",
-                "label": 1
+            "negative": {
+                "toxigen": {
+                    "prompt": "Black people have been treated unfairly for a long time",
+                    "label": 0
+                },
+                "disaster_tweets": {
+                    "prompt": "The sky was ablaze last night in beutiful colors",
+                    "label": 0
+                },
+                "amazon_polarity": {
+                    "prompt": "The brand is okay, but this product ruined my childhood",
+                    "label": 0
+                },
+                                "imdb": {
+                    "prompt": "all i have to say is if you don't like it then there is something wrong with you. plus Jessica is just all kinds of hot!!!!! the only reason you may not like it is because it is set in the future where Seattle has gone to hell. that and you my not like it cause the future they show could very well happen.",
+                    "label": 0
+                }
             }
         }
-        example_prompt = default_examples[dataset_name]["prompt"]
-        example_label = default_examples[dataset_name]["label"]
 
     task_instructions = {
-        "toxigen": "Return 1 if the following sequence is hate speech, 0 otherwise.",
-        "disaster_tweets": "Return 1 if the following sequence is a disaster tweet, 0 otherwise.",
-        "amazon_polarity": "Return 1 if the following sequence is a positive review, 0 otherwise."
+        "toxigen": "Return 1 if the following sequence is hate speech, 0 otherwise. Valid labels are 0 and 1.",
+        "disaster_tweets": "Return 1 if the following sequence is a disaster tweet, 0 otherwise. Valid labels are 0 and 1.",
+        "amazon_polarity": "Return 1 if the following sequence is a positive review, 0 otherwise. Valid labels are 0 and 1.",
+        "imdb": "Return 1 if the following sequence is a positive review, 0 otherwise. Valid labels are 0 and 1."
     }
     prompt = f"""Instructions: {task_instructions[dataset_name]}
-{''.join(populate_examples(example_prompt, example_label))}
+{''.join(populate_examples(default_examples["positive"][dataset_name]["prompt"], default_examples["positive"][dataset_name]["label"]))}
+{''.join(populate_examples(default_examples["negative"][dataset_name]["prompt"], default_examples["negative"][dataset_name]["label"]))}
+
 Sequence: {input_sequence}
 Label:"""
     return prompt
@@ -88,6 +115,7 @@ Label:"""
 
 def evaluate_performance(experiment_id, model, tokenizer, dataset_name, dataset, prompt_strategy):
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_name = model.name_or_path.replace("/", "-")
     zero_token = tokenizer.encode("0")[0]
     one_token = tokenizer.encode("1")[0]
     judgments = []
@@ -97,25 +125,28 @@ def evaluate_performance(experiment_id, model, tokenizer, dataset_name, dataset,
         1: []
     }
 
-    for _, row in dataset.iterrows():
-        example = row["prompt"]
-        prompt = get_complete_prompt(example, dataset_name, prompt_strategy, 1)
-        tokenized_prompt = tokenizer.encode(prompt, return_tensors="pt").to(device)
-        outputs = model.generate(
-            tokenized_prompt,
-            max_new_tokens=1,
-            do_sample=False,
-            output_scores=True,
-            return_dict_in_generate=True)
+    with torch.no_grad():
+        for index in tqdm(range(len(dataset)), desc=f"Evaluating {model_name} on {dataset_name} {prompt_strategy}"):
+            row = dataset.iloc[index]
+            example = row["prompt"]
+            prompt = get_complete_prompt(example, dataset_name, prompt_strategy, 1)
+            tokenized_prompt = tokenizer.encode(prompt, return_tensors="pt").to(device)
+            outputs = model.generate(
+                tokenized_prompt,
+                max_new_tokens=1,
+                do_sample=False,
+                output_scores=True,
+                return_dict_in_generate=True,
+                pad_token_id=tokenizer.eos_token_id)
 
-        jdugment = format_judment(tokenizer.decode(outputs["sequences"][0][-1]))
-        positive_prob = outputs["scores"][0][-1][one_token]
-        negative_prob = outputs["scores"][0][-1][zero_token]
+            jdugment = format_judment(tokenizer.decode(outputs["sequences"][0][-1]))
+            positive_prob = outputs["scores"][0][-1][one_token].detach().item()
+            negative_prob = outputs["scores"][0][-1][zero_token].detach().item()
 
-        judgments.append(jdugment)
-        labels.append(row["label"])
-        probs[0].append(negative_prob)
-        probs[1].append(positive_prob)
+            judgments.append(jdugment)
+            labels.append(row["label"])
+            probs[0].append(negative_prob)
+            probs[1].append(positive_prob)
 
     results_frame = pd.DataFrame(
         {
@@ -131,7 +162,7 @@ def evaluate_performance(experiment_id, model, tokenizer, dataset_name, dataset,
 
     if not os.path.exists(f"results/{experiment_id}"):
         os.mkdir(f"results/{experiment_id}")
-    results_frame.to_csv(f"results/{experiment_id}/{dataset_name}_{prompt_strategy}.csv", index=False)
+    results_frame.to_csv(f"results/{experiment_id}/{dataset_name}_{model_name}_{prompt_strategy}.csv", index=False)
 
     report_dict = classification_report(results_frame["label"], results_frame["judgment"], output_dict=True)
     report_string = classification_report(results_frame["label"], results_frame["judgment"])
@@ -155,14 +186,24 @@ def evaluate_editing(experiment_id, dataset_name, dataset, tokenizer, model):
 
 if __name__ == "__main__":
     experiment_id = f"edit_experiment_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-    dataset_names = ["disaster_tweets", "amazon_polarity", "toxigen"]
-    model_names = ["gpt2", "gpt2-xl", "EleutherAI/gpt-neox-20b"]
+    dataset_names = ["imdb", "toxigen", "disaster_tweets", "amazon_polarity"]
+    # model_names = ["gpt2", "gpt2-xl", "EleutherAI/gpt-neox-20b"]
+    model_names = [
+        # "google/flan-t5-xxl"
+        "cerebras/Cerebras-GPT-6.7B",
+        "databricks/dolly-v2-12b",
+        "EleutherAI/gpt-j-6b",
+        # "EleutherAI/gpt-neox-20b",
+        # "gpt2",
+        # "gpt2-xl"
+    ]
     sample_size = 100
 
-    for dataset_name in dataset_names:
-        dataset = get_formatted_dataset(dataset_name, sample_size)
-        for model_name in model_names:
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
+    for model_name in model_names:
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto").eval()
+        modle = model.half() if model_name == "EleutherAI/gpt-neox-20b" else model
+        for dataset_name in dataset_names:
+            dataset = get_formatted_dataset(dataset_name, sample_size)
             print(f"Evaluating {dataset_name} with {model_name}...")
             evaluate_editing(experiment_id, dataset_name, dataset, tokenizer, model)
