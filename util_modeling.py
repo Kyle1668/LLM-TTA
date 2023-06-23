a976f93f2a4fb273384537021c24239528bf76e0
Kyle1668 <kyledevinobrien1@gmail.com>
2023-06-23 23:53:48 +0000

    Load all modls in either fp16 for inference and fp32 for training

--- util_modeling.py
+++ util_modeling.py
@@ -27,6 +27,7 @@ def get_model_objects(model_name, num_labels, training=False):
 
     model = None
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    numerical_precision = torch.float32 if training else torch.float16
     if is_llm:
         num_billions = [int(entry[:-1]) for entry in model_name.split("-") if entry[0].isdigit() and entry.lower().endswith("b")]
         load_in_8bit = (len(num_billions) > 0 and num_billions[0] > 7) and not training
@@ -34,12 +35,11 @@ def get_model_objects(model_name, num_labels, training=False):
             print("Loading in 8-bit mode since the model has more than 7B parameters")
             model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, load_in_8bit=True, llm_int8_threshold=0, device_map="auto").eval()
         else:
-            numerical_precision = torch.float32 if training else torch.bfloat16
             model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, torch_dtype=numerical_precision).eval().to(device)
     elif is_qa_model:
-        model = AutoModelForQuestionAnswering.from_pretrained(model_name, trust_remote_code=True, torch_dtype=torch.float16).eval().to(device)
+        model = AutoModelForQuestionAnswering.from_pretrained(model_name, trust_remote_code=True, torch_dtype=numerical_precision).eval().to(device)
     elif is_seq2seq_lm:
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True).eval().to(device)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True, torch_dtype=numerical_precision).eval().to(device)
     else:
-        model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True, num_labels=num_labels).eval().to(device)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True, num_labels=num_labels, torch_dtype=numerical_precision).eval().to(device)
     return tokenizer, model
